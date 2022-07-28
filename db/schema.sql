9a7a1d4ea6d5b30be9cc58c26ac2b6a1c6bc1333
joostyn <justyn.helgeson@gmail.com>
2022-07-28 18:42:47 -0500

    wrote schema.sql and seed.sql

--- db/schema.sql
+++ db/schema.sql
@@ -13,8 +13,8 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    dept_id INT,
-    FOREIGN KEY (dept_id) REFERENCES department(id) on DELETE CASCADE
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) on DELETE CASCADE
 );
 
 CREATE TABLE employee (
